b2fb33375631ff6380281b64a5030b68a7d2d24f
Hans Bregman <hns101@live.nl>
2025-08-27 13:42:31 +0200

    added studentlist.vue

---
+++ src/components/StudentList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-3xl font-semibold text-gray-800">Ledenboek</h1>
+    <p class="mt-2 text-gray-600">This page will show a list of all students.</p>
+  </div>
+</template>
+
+<script setup>
+// This will be filled with logic to fetch students
+</script>
+    
