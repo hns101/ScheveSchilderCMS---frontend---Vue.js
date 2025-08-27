b9a6b479a736231180099cd3db3b2b65b8c63bce
Hans Bregman <hns101@live.nl>
2025-08-27 13:45:04 +0200

    added InvoiceBatch.vue

---
+++ src/components/InvoiceBatch.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-3xl font-semibold text-gray-800">Incasso boekingen</h1>
+    <p class="mt-2 text-gray-600">This page will be used to generate invoices in batches.</p>
+  </div>
+</template>
+
+<script setup>
+// This will be filled with logic to generate invoices
+</script>
